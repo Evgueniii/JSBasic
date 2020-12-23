e83f98f686a523c75c25ff244a1661cd051fc323
Evgueniii <evgueniii@mail.ru>
2020-12-23 13:43:13 +0300

    Буду честен, в 3-ем задании заковырялся и бессовестно адаптировал Ваше решение под свою корзину.

---
+++ 3.js
@@ -0,0 +1,71 @@
+const cartItem = {
+    render(good) {
+        return `<div class="good">
+                    <div><b>Наименование</b>: ${good.product_name}</div>
+                    <div><b>Цена за шт.</b>: ${good.price}</div>
+                    <div><b>Количество</b>: ${good.quantity}</div>
+                    <div><b>Стоимость</b>: ${good.quantity * good.price}</div>
+                </div>`;
+    }
+}
+
+const cart = {
+    cartListBlock: null,
+    cartButton: null,
+    cartItem,
+    goods: [
+      {
+        id_product: 001,
+        product_name: "Блендер",
+        price: 5800,
+        quantity: 1
+      },
+      {
+        id_product: 002,
+        product_name: "Соковыжималка",
+        price: 12850,
+        quantity: 1
+      },
+      {
+        id_product: 003,
+        product_name: "Смартфон",
+        price: 84500,
+        quantity: 1
+      },
+      {
+        id_product: 004,
+        product_name: "Фитнес браслет",
+        price: 15250,
+        quantity: 1
+      },
+    ],
+    init() {
+        this.cartListBlock = document.querySelector('.cart-list');
+        this.cartButton = document.querySelector('.cart-btn');
+        this.cartButton.addEventListener('click', this.clearCart.bind(this));
+        // this.cartButton.addEventListener('click', () => this.clearCart());
+
+        this.render();
+    },
+    render() {
+        if (this.goods.length) {
+            this.goods.forEach(good => {
+                this.cartListBlock.insertAdjacentHTML('beforeend', this.cartItem.render(good));
+            });
+            this.cartListBlock.insertAdjacentHTML('beforeend', `В корзине ${this.goods.length} позиций(а) стоимостью ${this.getCartPrice()}`);
+        } else {
+            this.cartListBlock.textContent = 'Корзина пуста';
+        }
+    },
+    getCartPrice() {
+        return this.goods.reduce(function (price, good) {
+            return price + good.price * good.quantity;
+        }, 0);
+    },
+    clearCart() {
+        this.goods = [];
+        this.render();
+    },
+};
+
+cart.init();
